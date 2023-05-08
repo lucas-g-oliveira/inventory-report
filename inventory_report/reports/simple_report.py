42e9cbd8f2281123f1f3b9e8449ece904d9e5d6c
Lucas G <lucasg.devmail93@gmail.com>
2023-05-08 20:42:11 -0300

    feat: implemented function SimpleReport.generate

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(array: list):
+        def toDate(string: str):
+            return datetime.strptime(string, "%Y-%m-%d").date()
+
+        factored = [toDate(date["data_de_fabricacao"]) for date in array]
+        validate = [toDate(date["data_de_validade"]) for date in array]
+        companys = Counter([comp["nome_da_empresa"] for comp in array])
+
+        fact = min(factored).strftime("%Y-%m-%d")
+        val = max(validate).strftime("%Y-%m-%d")
+        comp = companys.most_common(1)[0]
+
+        return f"""
+        Data de fabricação mais antiga: {fact}
+        Data de validade mais próxima: {val}
+        Empresa com mais produtos: {comp[0]}"""
