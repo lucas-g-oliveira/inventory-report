ef1d4e3441b86dede7c26b4bca3dbe8df94e5d74
Lucas G <lucasg.devmail93@gmail.com>
2023-05-08 16:17:07 -0300

    created test for contructor of class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,31 @@
-# from inventory_report.inventory.product import Product
+import pytest
+import datetime
+from inventory_report.inventory.product import Product
 
 
+@pytest.xfail
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+
+    prod1 = Product(
+        1,
+        "bol",
+        "magic toys",
+        datetime.now(),
+        datetime.date(2100, 5, 15),
+        "1223356648899",
+        "armazene em lugar arejado com temperatura até 60Cº",
+    )
+
+    attributes = [
+        "nome_da_empresa",
+        "nome_do_produto",
+        "data_de_fabricacao",
+        "data_de_validade",
+        "numero_de_serie",
+        "instrucoes_de_armazenamento",
+    ]
+
+    assert type(prod1.id) == int
+
+    for attr in attributes:
+        assert type(prod1[attr]) == str
