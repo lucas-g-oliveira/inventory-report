45492169a22380910a3620d97cb274f20ea9abfd
Lucas G <lucasg.devmail93@gmail.com>
2023-05-09 16:14:22 -0300

    fix: simple_reports

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,53 @@
-class CompleteReport:
-    pass
+from simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(array: list):
+        smpl = SimpleReport.generate(array)
+        complete = "\nProdutos estocados por empresa:"
+        prod_per_company = Counter([comp["nome_da_empresa"] for comp in array])
+        companys = []
+
+        for i in array:
+            if not (i["nome_da_empresa"]) in companys:
+                companys.append(i["nome_da_empresa"])
+
+        for comp in companys:
+            complete += f"\n- {comp}: {prod_per_company[comp]}"
+
+        return smpl + complete
+
+
+data = [
+    {
+        "id": 1,
+        "nome_do_produto": "CADEIRA",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2022-05-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "Conservar em local fresco",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "CADEIRA",
+        "nome_da_empresa": "Toy",
+        "data_de_fabricacao": "2012-04-04",
+        "data_de_validade": "2023-05-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "Conservar em local fresco",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "CADEIRA",
+        "nome_da_empresa": "Toy",
+        "data_de_fabricacao": "2020-04-04",
+        "data_de_validade": "2021-02-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "Conservar em local fresco",
+    },
+]
+
+print(CompleteReport.generate(data))
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -11,12 +11,19 @@ class SimpleReport:
         factored = [toDate(date["data_de_fabricacao"]) for date in array]
         validate = [toDate(date["data_de_validade"]) for date in array]
         companys = Counter([comp["nome_da_empresa"] for comp in array])
+        validate.sort()
+        val = ""
+
+        for d in validate:
+            if d >= datetime.today().date():
+                val = d.strftime("%Y-%m-%d")
+                break
 
         fact = min(factored).strftime("%Y-%m-%d")
-        val = max(validate).strftime("%Y-%m-%d")
-        comp = companys.most_common()[0]
+        comp = companys.most_common()[0][0]
 
-        return f"""
-        Data de fabricação mais antiga: {fact}
-        Data de validade mais próxima: {val}
-        Empresa com mais produtos: {comp}"""
+        result_p1 = f"Data de fabricação mais antiga: {fact}\n"
+        result_p2 = f"Data de validade mais próxima: {val}\n"
+        result_p3 = f"Empresa com mais produtos: {comp}"
+
+        return result_p1 + result_p2 + result_p3
