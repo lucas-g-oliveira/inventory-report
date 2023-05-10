30d5c2a1e2ddab9b2bca2d4a617efc358979d095
Lucas G <lucasg.devmail93@gmail.com>
2023-05-10 16:09:04 -0300

    fix: function __from_xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,7 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import json
-# import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
 import csv
 
 
@@ -10,11 +10,26 @@ class Inventory:
         with open(path, "r") as file:
             return [*csv.DictReader(file)]
 
-    """ @staticmethod
     def __from_xml(path):
-        file = ET.parse(path).getroot()
-        data = {}
-        for i in file:
+        file = ET.parse(path)
+        root = file.getroot()
+        o_i = 0
+        data = []
+        for i in root:
+            k_i = 0
+            obj_temp = {}
+            for _key in i:
+                obj_temp[root[o_i][k_i].tag] = root[o_i][k_i].text
+                k_i += 1
+            data.append(obj_temp)
+            o_i += 1
+        return data
+
+        """ print("_______________________________________________")
+        print(root[0][0].tag, root[0][0].text)
+        print("_______________________________________________")
+        return [] """
+        """  for i in file:
             data[i.tag] = i.text
         return data """
 
@@ -30,16 +45,20 @@ class Inventory:
         }
         type_file = {
             "json": cls.__from_json,
-            #  "xml": cls.__from_xml,
+            "xml": cls.__from_xml,
             "csv": cls.__from_csv,
         }
         file = type_file[path.split(".")[-1]](path)
         return report[type_report](file)
 
 
-# csv_p1 = "/home/lucas/Documentos/1-Trybe/3-Projetos-Trybe/30 - "
-# csv_p2 = "Inventory Report/sd-024-b-inventory-report"
-# csv_p3 = "/inventory_report/data/inventory.csv"
+p1 = "/home/lucas/Documentos/1-Trybe/3-Projetos-Trybe/30 - Inventory Report/"
+p2 = "sd-024-b-inventory-report/inventory_report/data/inventory"
+# p3 = ".csv"
+p3 = ".xml"
+# p3 = ".json"
 
-# print(Inventory.import_data(f"{csv_p1}{csv_p2}{csv_p3}", "simples"))
-# print(Inventory.import_data(f"{csv_p1}{csv_p2}{csv_p3}", "completo"))
+
+path = p1 + p2 + p3
+print(Inventory.import_data(f"{path}", "simples"))
+print(Inventory.import_data(f"{path}", "completo"))
