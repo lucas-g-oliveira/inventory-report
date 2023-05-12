d0f49dbf61551375247fbf9f49292bf9f0e592cb
Lucas G <lucasg.devmail93@gmail.com>
2023-05-12 10:40:02 -0300

    feat: created test for tag to stock

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    values = [
+        1,
+        "bol",
+        "magic toys",
+        "2022-02-22",
+        "2023-07-30",
+        "1223356648899",
+        "armazene em lugar arejado com temperatura até 60Cº",
+    ]
+
+    prod = Product(*values)
+
+    assert str(prod) == (
+        f"O produto {values[1]}"
+        f" fabricado em {values[3]}"
+        f" por {values[2]} com validade"
+        f" até {values[4]}"
+        f" precisa ser armazenado {values[6]}."
+    )
