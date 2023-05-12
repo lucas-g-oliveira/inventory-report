102d39ddbd9659f21239bc40448cbd1f6c160940
Lucas G <lucasg.devmail93@gmail.com>
2023-05-11 21:02:24 -0300

    feat: created test to class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,9 +1,8 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass
-    """ p1 = Product(
+    values = [
         1,
         "bol",
         "magic toys",
@@ -11,37 +10,14 @@ def test_cria_produto():
         "2023-07-30",
         "1223356648899",
         "armazene em lugar arejado com temperatura até 60Cº",
-    )
-
-    p2 = Product(
-        "",
-        "car",
-        "magic toys",
-        "2012-02-22",
-        "2024-07-30",
-        "",
-        "armazene em lugar arejado com temperatura até 60Cº",
-    )
-
-    keys = [
-        "nome_da_empresa",
-        "nome_do_produto",
-        "data_de_fabricacao",
-        "data_de_validade",
-        "numero_de_serie",
-        "instrucoes_de_armazenamento",
     ]
 
-    p1_assert = []
-    p2_assert = []
+    prod = Product(*values)
 
-    p1_assert.append(type(p1.id) is int)
-    p1_assert.extend(type(p1[key]) == str for key in keys)
-
-    p2_assert.append(type(p2.id) is int)
-    p2_assert.extend(type(p2[key]) == str for key in keys)
-
-    assert all(p1_assert) is True
-
-    assert any(p2_assert) is False
- """
+    assert values[0] is prod.id
+    assert values[1] is prod.nome_do_produto
+    assert values[2] is prod.nome_da_empresa
+    assert values[3] is prod.data_de_fabricacao
+    assert values[4] is prod.data_de_validade
+    assert values[5] is prod.numero_de_serie
+    assert values[6] is prod.instrucoes_de_armazenamento
